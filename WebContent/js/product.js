c41579ef682649db920cbdc3b4e705afea620c35
mayconghizoni <maycon_ghizoni@hotmail.com>
2019-09-19 20:26:37 -0300

    OT-09 Front-end Finalizado

--- WebContent/js/product.js
+++ WebContent/js/product.js
@@ -11,10 +11,50 @@ $(document).ready(function(){
             type: "GET",
             url: "/ProjetoTrilhaWeb/rest/marca/buscar",
             success: function(marcas) {
-                alert("Sucesso")
+                if (marcas!=""){
+
+
+                    //Cria opção escolha com valor vazio como padrão caso haja algo no banco de dados
+                    $("selMarca").html("")
+                    var option = document.createElement("option")
+                    option.setAttribute("value", "")
+                    option.innerHTML = ("Escolha")
+                    $("#selMarca").append(option)
+
+                    //A cada valor encontrado no banco, cria mais uma option dentro do select
+                    for (var i = 0; i < marcas.length; i++){
+
+                        var option = document.createElement("option")
+                        option.setAttribute("value", marcas[i].id)
+                        option.innerHTML = (marcas[i].nome)
+                        $("#selMarca").append(option)
+
+                    }
+
+
+                }else{
+                    $("selMarca").html("")
+
+                    //Caso o não tenha nenhum valor cadastrado no banco ele cria uma uma option com aviso!
+                    var option = document.createElement("option")
+                    option.setAttribute("value", "")
+                    option.innerHTML = ("Cadastre uma marca primeiro!")
+                    $("#selMarca").append(option)
+                    $("#selMarca").addClass("aviso")
+
+                }
             },
             error: function(info) {
-                alert("Errorrrrrrrrrrrrrrrrrrrrrrrrrrr")
+
+                COLDIGO.exibirAviso("Erro ao buscar marcas: "+info.status+" - " + info.statusText)
+
+                $("#selMarca").html("")
+                var option = document.createElement("option")
+                option.setAttribute("value", "")
+                option.innerHTML = ("Erro ao carregar marcas!")
+                $("#selMarca").append(option)
+                $("#selMarca").addClass("aviso")
+
             } 
         })
 
